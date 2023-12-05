7c5080607c7dca41f9d548187ad034a5f9384e83
Joseph <129715233+Jooi025@users.noreply.github.com>
2023-12-06 01:19:41 +1100

    Create capture.py

---
+++ capture.py
@@ -0,0 +1,21 @@
+import cv2 as cv
+from time import time
+from modules.windowcapture import WindowCapture
+from constants import Constants
+
+# initialize the WindowCapture class
+wincap = WindowCapture(Constants.window_name)
+
+while(True):
+    # get an updated image of the game
+    screenshot = wincap.get_screenshot()
+
+    cv.imshow("YOLOv8", screenshot)
+    loop_time = time()
+
+    # press 'q' with the output window focused to exit.
+    key = cv.waitKey(1)
+    if key == ord('q'):
+        cv.destroyAllWindows()
+        break
+print('Done.')
